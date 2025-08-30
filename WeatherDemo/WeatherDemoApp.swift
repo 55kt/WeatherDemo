d7f669b9ce8da452689c7f85d751b621dcceb74a
Vladyslav R <160039489+55kt@users.noreply.github.com>
2025-08-30 21:41:29 +0300

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Vlad on 30/8/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
